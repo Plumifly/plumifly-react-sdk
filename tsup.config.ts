115db3e359c9a8a8fe294bcad7d940902db69a60
Alberto Slavica <albertoslavica19@gmail.com>
2024-12-30 14:15:42 +0900

    Preparing 0.1.0 Release

--- tsup.config.ts
+++ tsup.config.ts
@@ -6,6 +6,7 @@ export default defineConfig({
   dts: true,
   clean: true,
   external: ['react', 'react-dom'],
+  outDir: 'dist',
   outExtension({ format }) {
     return {
       js: format === 'cjs' ? '.js' : '.mjs'
